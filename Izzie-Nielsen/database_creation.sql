fc8eb64044595624500519f3d98dac7bbcd134d6
Isabella Nielsen <95454109+IzzieNielsen@users.noreply.github.com>
2025-09-30 22:40:04 -0500

    Update database_creation.sql
    I accidentally selected the wrong file.

--- Izzie-Nielsen/database_creation.sql
+++ Izzie-Nielsen/database_creation.sql
@@ -1,10 +1,23 @@
-CREATE DATABASE music_database;
+-- only run once to create the database
+-- CREATE DATABASE menu_recommender
 
-CREATE TABLE user_data (
-	artist VARCHAR(50),
-    id INT PRIMARY KEY,
-    title VARCHAR(255)
-)
+-- run to start programming in the database if using MySQL shell
+-- USE menu_recommender
+
+-- creates the table for recipes 
+CREATE TABLE recipes(
+	RECIPE_NAME VARCHAR(50),
+    INGREDIENTS VARCHAR(1024),
+    ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+);
+
+-- change file path to match your path
+-- loads data from csv into the recipes table
+LOAD DATA INFILE '/Users/izzienielsen/Documents/CSC Capstone/github downloads/recipe_info.csv'
+INTO TABLE recipes 
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
 
 
 
